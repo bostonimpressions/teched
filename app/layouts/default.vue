981b305ee968e2096431538679973e84818471bc
Boston Impressions <bostonimpressions@gmail.com>
2025-09-03 11:29:42 -0400

    add title and meta

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,32 @@
-<script setup></script>
+<script setup lang="ts">
+  useHead({
+    title:
+      'Career and Technical Education Training | TechEd Products | United States',
+    meta: [
+      {
+        name: 'description',
+        content:
+          'With over 50 years combined experience serving New England, TechEd Products is the area’s most experienced independent educational sales consultant organization.',
+      },
+      {
+        property: 'og:title',
+        content:
+          'Career and Technical Education Training | TechEd Products | United States',
+      },
+      {
+        property: 'og:description',
+        content:
+          'With over 50 years combined experience serving New England, we are the area’s most experienced independent educational sales consultant organization. By combining our local knowledge and relationships with our partners, we provide the best solutions to prepare your students or trainees with critical STEM and job ready skills required in today’s technical workforce.',
+      },
+    ],
+    link: [
+      {
+        rel: 'canonical',
+        href: 'https://www.techedproducts.com',
+      },
+    ],
+  })
+</script>
 
 <template>
   <main>
